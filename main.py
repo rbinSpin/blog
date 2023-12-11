d10b7e0dfa6d519f8cb2d24f692f25fa229e6ac2
rbin <steven2000516@gmail.com>
2023-12-12 01:03:22 +0800

    after code cleaning 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 from starlette import status
 from models import article
@@ -11,11 +11,11 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/web_page", status_code=status.HTTP_302_FOUND)
+
 app.include_router(article.router)
 app.include_router(web_page.router)
-
-# redirect to /web_page
-@app.get("/", response_class=HTMLResponse)
-async def return_to_index():
-    return RedirectResponse(url="/web_page", status_code=status.HTTP_302_FOUND)
 
